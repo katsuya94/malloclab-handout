a6478a7690f0d0e16ba420f2ed71d0d4d1f07840
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2013-12-05 21:29:25 -0600

    ALL OF THE COMMENTS

--- mm.c
+++ mm.c
@@ -1,11 +1,12 @@
 /*
- * mm.c - Implements a LIFO Explicit Free Block List with Bidirectional Coalescing.
+ * mm.c - Implements free block lists sorted into sizes between exponents of 2 with bidirectional coalescing
  * 
  * Block structure is as follows:
  * The first SIZE_T_SIZE bytes are the header.
  * The last SIZE_T_SIZE bytes are the footer.
  * The last 2 bits of the header and the footer are the allocation flag.
  * These bits are usable because the sizes are aligned bt 8 bytes.
+ * The payload is the bytes between the header and footer.
  *
  * flag == 0x0
  *     FREE
@@ -14,10 +15,24 @@
  * flag == 0x2
  *     ROOT
  *
- * The root flag is used to stop attempted access below the heap.
+ * The root flag is used to prevent modifying bookkeeping information.
  * The root block is the block at the bottom of the heap.
- * The payload is the bytes between the header and footer.
  *
+ * The first free list accommodates block sizes up to twice MIN_BLK_SIZE.
+ * This pattern continues until LIST_MAX_SIZE(n) surpasses the limit of size_t.
+ *
+ * The free list root blocks are accessed rith the ROOT(n) macro.
+ * Since the root block attribute that matters is NEXT, they are stored overlapping each other.
+ * The first SIZE_T_SIZE bytes in the heap are the header of the root block.
+ * The next PTR_SIZE bytes are the NEXT of ROOT(0).
+ * Every PTR_SIZE chunk following that is the NEXT of ROOT(n),
+ * until the last NEXT, after which is the footer of SIZE_T_SIZE.
+ * This concludes the static bookkeeping information in the heap.
+ *
+ * The package is guaranteed to:
+ *     Never have adjacent free blocks
+ *     Never have a free block in the wrong list
+ *     Never leave behind space that can never be reused
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -45,6 +60,11 @@ team_t team = {
 	"nathanyeazel2016@u.northwestern.edu"
 };
 
+/*
+ * When MM_CHECK is defined, mm_check is called at the end of every mm_init, mm_malloc, and mm_free call
+ * When VERBOSE is defined, every mm_malloc and mm_free call prints its calling conditions.
+ *     Additionally, every call to mm_check will print the contents of the heap and free lists.
+ */
 //#define MM_CHECK
 //#define VERBOSE
 
@@ -54,65 +74,72 @@ team_t team = {
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
+/* Aligned sizes of common types */
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
-
 #define PTR_SIZE (ALIGN(sizeof(void *)))
 
+/* Minimum size of a valid block */
 #define MIN_BLK_SIZE (SIZE_T_SIZE*2 + PTR_SIZE*2)
 
-/* packs size and allocation bit, assuming that size is aligned */
+/* Packs size and allocation bit, assuming proper alignment */
 #define PACK(size, alloc) ((size) | (alloc))
 
+/* Gets payload from block */
 #define PAYLOADP(bp) ((void *)((char *)bp + SIZE_T_SIZE))
-#define BLOCK(pp) ((void *)((char *)pp - SIZE_T_SIZE))
+/* Gets block from payload */
+#define BLOCKP(pp) ((void *)((char *)pp - SIZE_T_SIZE))
 
+/* Accesses components of a header */
 #define BLK_SIZE(bp) (HDR(bp) & ~0x3)
 #define BLK_ALLOC(bp) (HDR(bp) & 0x3)
 
+/* Accesses the header and footer of a block */
 #define HDR(bp) (*((size_t *)bp))
 #define FTR(bp) (*((size_t *)((char *)bp + BLK_SIZE(bp) - SIZE_T_SIZE)))
 
+/* Acceses the next/previous block in a free list */
 #define NEXT(bp) (*((void **)((char *)bp + SIZE_T_SIZE)))
 #define PREV(bp) (*((void **)((char *)bp + SIZE_T_SIZE + PTR_SIZE)))
 
+/* Accesses the next/previous block on the heap */
 #define LIN_NEXT(bp) ((void *)((char *)bp + BLK_SIZE(bp)))
 #define LIN_PREV(bp) ((void *)((char *)bp - (*((size_t *)((char *)bp - SIZE_T_SIZE)) & ~0x3)))
 
-#define NUM_CLASSES (sizeof(size_t)*8-5)
-#define CLASS_SIZE(n) (0x40*(1 << n))
+/* Information about the free lists */
+#define NUM_LISTS (sizeof(size_t)*8-5)
+#define LIST_MAX_SIZE(n) (0x40*(1 << n))
 
-#define ROOT(n) ((void *)((char *) mm_roots + SIZE_T_SIZE * n))
-
-/* globals
- * mm_root - Pointer to the root block, located at the bottom of the heap
- */
-void *mm_roots;
-
-static void mm_setbounds(void *blkp, size_t value);
-static void mm_setbounds(void *blkp, size_t value);
-static void mm_remove(void *blkp);
-static void mm_insert(void *dest, void *blkp);
-static void *mm_coalesce(void *ptr);
-static void *mm_traverse(size_t newsize, void *root);
-static void mm_putList(void *ptr);
-static void *mm_append(size_t newsize, void *root);
-static void *mm_findSpace(size_t newsize);
-void dumpclasses(void);
+#define ROOT(n) ((void *)((char *) mem_heap_lo() + SIZE_T_SIZE * n))
 
 /*
- * mm_remove - Sets the header and footer of a block to value
+ * Function Prototypes
+ */
+static int mm_check(void);
+static void mm_printLists(void);
+static void *mm_append(size_t newsize, void *root);
+static void *mm_findSpace(size_t newsize);
+static void *mm_unfloat(void *ptr);
+static void *mm_traverse(size_t newsize, void *root);
+static void mm_putList(void *ptr);
+static void mm_insert(void *dest, void *blkp);
+static void mm_remove(void *blkp);
+static void mm_setBounds(void *blkp, size_t value);
+
+/*
+ * mm_setBounds - Sets the header and footer of a block to value
  *     Note that size also includes the allocation flag
  *     Common usage passes the result of PACK(size, alloc) to value
+ *     It is imprtant that the header is set first, as a proper header is required to find the footer
  */
-static void mm_setbounds(void *blkp, size_t value)
+static void mm_setBounds(void *blkp, size_t value)
 {
 	HDR(blkp) = value;
 	FTR(blkp) = value;
 }
 
 /*
- * mm_remove - For a consistent stack, this removes a FREE block from the chain
- *     Behavior is undefined for non FREE blocks
+ * mm_remove - Removes a FREE block from the chain
+ *     Behavior is undefined for floating blocks or root blocks.
  */
 static void mm_remove(void *blkp)
 {
@@ -122,10 +149,9 @@ static void mm_remove(void *blkp)
 }
 
 /*
- * mm_insert - Inserts a FREE block in place of a specified non NULL block
- *     Behavior is undefined for non FREE blocks
+ * mm_insert - Inserts a block after a specified block
+ *     Behavior is only defined for floating blocks.
  */
-
 static void mm_insert(void *dest, void *blkp)
 {
 	PREV(blkp) = dest;
@@ -135,33 +161,40 @@ static void mm_insert(void *dest, void *blkp)
 	NEXT(dest) = blkp;
 }
 
-// Assumes that ptr is floating
-static void *mm_coalesce(void *ptr)
+/*
+ * mm_unfloat - Performs all the necessary operations to place a floating block in the heap, preserving validity
+ *     Behavior is only defined for floating blocks.
+ *     Returns a pointer to the input block, possibly coalesced with the immediately preceding and following blocks.
+ */
+static void *mm_unfloat(void *blkp)
 {
-	void *next = LIN_NEXT(ptr);
-	void *prev = LIN_PREV(ptr);
+	void *next = LIN_NEXT(blkp);
+	void *prev = LIN_PREV(blkp);
 
 	if(next < mem_heap_hi()+1 && BLK_ALLOC(next) == 0x0)
 	{
 		mm_remove(next);
-		mm_setbounds(ptr, PACK(BLK_SIZE(ptr) + BLK_SIZE(next), 0x0));
+		mm_setBounds(blkp, PACK(BLK_SIZE(blkp) + BLK_SIZE(next), 0x0));
 	}
 	if(BLK_ALLOC(prev) == 0x0)
 	{
 		mm_remove(prev);
-		mm_setbounds(prev, PACK(BLK_SIZE(prev) + BLK_SIZE(ptr), 0x0));
+		mm_setBounds(prev, PACK(BLK_SIZE(prev) + BLK_SIZE(blkp), 0x0));
 		mm_putList(prev);
 		return prev;
 	}
 	else
 	{
-		mm_putList(ptr);
-		return ptr;
+		mm_putList(blkp);
+		return blkp;
 	}
 
 }
 
-static void __attribute__ ((noinline)) *mm_traverse(size_t newsize, void *root)
+/*
+ * mm_traverse - Searches a free block list and returns the first block that fits newsize or NULL
+ */
+static void *mm_traverse(size_t newsize, void *root)
 {
 	void *blkp = NEXT(root);
 	void *found = NULL;
@@ -171,25 +204,26 @@ static void __attribute__ ((noinline)) *mm_traverse(size_t newsize, void *root)
 			found = blkp;
 		blkp = NEXT(blkp);
 	}
-
 	return found;
 }
 
-// this finds the list that fits a specific block, and then puts that block into the list. 
-//Redo this code
+/*
+ * mm_putList - Takes a floating block and puts in the appropriate list
+ *     Behavior is only defined for floating blocks.
+ *     Guaranteed to find an appropriate list.
+ */
 static void mm_putList(void *ptr)
 {
-	int i;
-	for(i = 0; i < NUM_CLASSES; i++)
-	{	
-		if(CLASS_SIZE(i) > BLK_SIZE(ptr))
-		{
-			mm_insert(ROOT(i), ptr);
-			return;
-		}
-	}
+	int i = 0;
+	for(i; LIST_MAX_SIZE(i) < BLK_SIZE(ptr); i++);
+	mm_insert(ROOT(i), ptr);
 }
 
+/*
+ * mm_append - Extends the stack to fit a new block
+ *     If there is a free block at the top of the heap, add to that block and return.
+ *     Otherwise, allocate two blocks of the requested size, and return the top block.
+ */
 static void *mm_append(size_t newsize, void *root)
 {
 	void *heaptail = LIN_PREV(mem_heap_hi()+1);
@@ -202,9 +236,9 @@ static void *mm_append(size_t newsize, void *root)
 		if (found == (void *)-1)
 			return NULL;
 
-		mm_setbounds(found, PACK(extension, 0x0));
+		mm_setBounds(found, PACK(extension, 0x0));
 
-		found = mm_coalesce(found);
+		found = mm_unfloat(found);
 	}
 	else
 	{
@@ -212,37 +246,39 @@ static void *mm_append(size_t newsize, void *root)
 		if (extra == (void *)-1)
 			return NULL;
 
-		mm_setbounds(extra, PACK(newsize, 0x0));
+		mm_setBounds(extra, PACK(newsize, 0x0));
 		mm_insert(root, extra);
 
 		found = mem_sbrk(newsize);
 		if (found == (void *)-1)
 			return NULL;
 
-		mm_setbounds(found, PACK(newsize, 0x0));
+		mm_setBounds(found, PACK(newsize, 0x0));
 		mm_insert(root, found);
 	}
 
 	return found;
 }
 
-// Redo this code to look cleaner
+/*
+ * mm_findSpace - Guaranteed to find a free block of size newsize or greater
+ */
 static void *mm_findSpace(size_t newsize)
 {
 	int i = 0;
-	void *returnVal = NULL;
+	void *returnval = NULL;
 
-	for(i; CLASS_SIZE(i) <= newsize; i++);
-
+	// Calculates and saves the first free list that should fit newsize
+	for(i; LIST_MAX_SIZE(i) < newsize; i++);
 	int first = i;
 
-	for(i = first; i < NUM_CLASSES; i++) //add >= again
+	for(i; i < NUM_LISTS; i++)
 	{
 		if(NEXT(ROOT(i)) != NULL)
-			returnVal = mm_traverse(newsize, ROOT(i));
+			returnval = mm_traverse(newsize, ROOT(i));
 
-		if (returnVal != NULL)
-			return returnVal;	
+		if (returnval != NULL)
+			return returnval;	
 	}
 
 	return mm_append(newsize, ROOT(first));
@@ -250,16 +286,17 @@ static void *mm_findSpace(size_t newsize)
 
 /* 
  * mm_init - Initialize the malloc package
+ *     Makes a root block that provides access to all the free lists in the heap.
  */
 int mm_init(void)
 {
-	size_t rootssize = MIN_BLK_SIZE+SIZE_T_SIZE*(NUM_CLASSES-1);
-	// Set up the size classes
-	mm_roots = mem_sbrk(rootssize);
+	size_t rootblocksize = MIN_BLK_SIZE+SIZE_T_SIZE*(NUM_LISTS-1);
+	if(mem_sbrk(rootblocksize) == (void *)-1)
+		return -1;
 	int i;
-	for(i = 0; i < NUM_CLASSES; i++)
+	for(i = 0; i < NUM_LISTS; i++)
 		NEXT(ROOT(i)) = NULL;
-	mm_setbounds(mem_heap_lo(), PACK(rootssize, 0x2));
+	mm_setBounds(mem_heap_lo(), PACK(rootblocksize, 0x2));
 
 #ifdef MM_CHECK
 	mm_check();
@@ -269,8 +306,8 @@ int mm_init(void)
 }
 
 /*
- * mm_malloc - LIFO Explicit Free List
- *     
+ * mm_malloc - Returns a pointer to the payload of a block
+ *     May split a larger free block for allocation. Relocates the tail free block.
  */
 void *mm_malloc(size_t size)
 {
@@ -287,17 +324,18 @@ void *mm_malloc(size_t size)
 	if(found == NULL)
 		return NULL;
 
+	// Float the block
 	mm_remove(found);
 
 	size_t tail_size = BLK_SIZE(found) - newsize;
 	if(tail_size < MIN_BLK_SIZE)
-		mm_setbounds(found, PACK(BLK_SIZE(found), 0x1));
+		mm_setBounds(found, PACK(BLK_SIZE(found), 0x1));
 	else
 	{
-		mm_setbounds(found, PACK(newsize, 0x1));
+		mm_setBounds(found, PACK(newsize, 0x1));
 
 		void *tail = LIN_NEXT(found);
-		mm_setbounds(tail, PACK(tail_size, 0x0));
+		mm_setBounds(tail, PACK(tail_size, 0x0));
 		
 		mm_putList(tail);
 	}
@@ -310,18 +348,18 @@ void *mm_malloc(size_t size)
 }
 
 /*
- * mm_free - No coalescing yet, just insert in the list.
+ * mm_free - Mark a block as free then unfloat it.
  */
-void mm_free(void *ptr)
+void mm_free(void *payload)
 {
 #ifdef VERBOSE
 	printf("\nmm_free(%#08x)\n", ptr);
 #endif
 
-	void *blkp = BLOCK(ptr);
-	mm_setbounds(blkp, PACK(BLK_SIZE(blkp), 0x0));
+	void *blkp = BLOCKP(payload);
+	mm_setBounds(blkp, PACK(BLK_SIZE(blkp), 0x0));
 
-	blkp = mm_coalesce(blkp);
+	blkp = mm_unfloat(blkp);
 
 #ifdef MM_CHECK
 	mm_check();
@@ -349,9 +387,9 @@ void *mm_realloc(void *ptr, size_t size)
 }
 
 /*
- * mm_check - Peforms checks on the consistency of the stack.
+ * mm_check - Peforms checks on the consistency of the stack. When VERBOSE is defined, prints heap contents.
  */
-int mm_check(void)
+static int mm_check(void)
 {
 	void *blkp;
 
@@ -359,7 +397,7 @@ int mm_check(void)
 	printf("\n");
 	for(blkp = mem_heap_lo(); blkp < mem_heap_hi() + 1; blkp = LIN_NEXT(blkp))
 		printf("%#08x 0x%x NEXT=0x%08x PREV=0x%08x size=%#x\n", blkp, BLK_ALLOC(blkp), NEXT(blkp), PREV(blkp), BLK_SIZE(blkp));
-	dumpclasses();
+	mm_printLists();
 #endif
 
 	blkp = mem_heap_lo();
@@ -377,17 +415,20 @@ int mm_check(void)
 	return 1;
 }
 
-void dumpclasses(void) {
-	int i;
+/*
+ * mm_printLists - Prints the status of all free lists. If list is in use, prints contents.
+ */
+static void mm_printLists(void) {
 	printf("\n");
-	for(i = 0; i < NUM_CLASSES; i++)
+	int i;
+	for(i = 0; i < NUM_LISTS; i++)
 	{
-		printf("#%2d Max: 0x%08x Status: ", i, CLASS_SIZE(i));
+		printf("#%2d Max: 0x%08x Status: ", i, LIST_MAX_SIZE(i));
 		if(NEXT(ROOT(i)) == NULL)
-				printf("Uninitialized\n");
+				printf("Empty\n");
 		else
 		{
-			printf("Initialized\n");
+			printf("In Use\n");
 			void* blkp = ROOT(i);
 			while(blkp != NULL)
 			{
